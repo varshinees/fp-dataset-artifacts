fixed loss computation

run.py
@@ -22,9 +22,14 @@ class EnsembleTrainer(Trainer):
         loss, outputs = super().compute_loss(model, inputs, True)
         bad_loss, bad_outputs = super().compute_loss(self.bad_model, inputs, True)
 
+        # take log softmax of outputs
+        log_softmax = torch.nn.LogSoftmax()
+        outputs = log_softmax(outputs.logits)
+        bad_outputs = log_softmax(bad_outputs.logits)
+
         # select the right probs from the outputs
-        outputs = outputs.logits.index_select(dim=0, index=inputs['labels'])
-        bad_outputs = torch.add(torch.neg(bad_outputs.logits), 1)
+        outputs = outputs.index_select(dim=0, index=inputs['labels'])
+        bad_outputs = torch.add(torch.neg(bad_outputs), 1)
         new_loss = torch.sum(torch.neg(bad_outputs * outputs))
 
         return torch.squeeze(new_loss)
@@ -88,7 +93,7 @@ def main():
 
     model_class = model_classes[args.task]
     # Initialize the model and tokenizer from the specified pretrained model/checkpoint
-    bad_model = model_class.from_pretrained('./trained_model_old', **task_kwargs)
+    bad_model = model_class.from_pretrained('./trained_model_bad', **task_kwargs)
     # Freeze bad model's params
     bad_model.requires_grad = False
 
@@ -177,8 +182,8 @@ def main():
     )
     # Train and/or evaluate
     if training_args.do_train:
-        # trainer.train(resume_from_checkpoint=True)
         trainer.train()
+        # trainer.train()
         trainer.save_model()
         # If you want to customize the way the loss is computed, you should subclass Trainer and override the "compute_loss"
         # method (see https://huggingface.co/transformers/_modules/transformers/trainer.html#Trainer.compute_loss).